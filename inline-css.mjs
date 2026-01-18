9e9795193b7dccb59748ae792a9581350f775555
Eric <ericmourinmarin@gmail.com>
2026-01-18 21:38:48 +0100

    Created a first template preprocessor version

--- inline-css.mjs
+++ inline-css.mjs
@@ -2,10 +2,18 @@ import fs from "fs";
 
 const srcXml = "./src/index.xml";
 const distXml = "./dist/index.xml";
-const cssPath = "./dist/styles.css";
+const cssPath = "./dist/output.css";
 
 const xml = fs.readFileSync(srcXml, "utf8");
 const css = fs.readFileSync(cssPath, "utf8");
+
+xml = xml.replace(
+    /<mono:include\s+file="(.+?)"\s*\/>/g,
+    (_, filePath) => {
+        const fullPath = `./src/${filePath}`;
+        return fs.readFileSync(fullPath, "utf8");
+    }
+);
 
 const skinBlock = `
 <b:skin><![CDATA[
